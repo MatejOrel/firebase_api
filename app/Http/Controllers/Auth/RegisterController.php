00d86acbae1d355459e23749e49465259663dfdc
Anže Oblak <anze.oblak2@gmail.com>
2021-01-08 17:14:23 +0100

    Register Controller

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -6,28 +6,10 @@ use Auth;
 use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
-use Illuminate\Foundation\Auth\RegistersUsers;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Validator;
 use Kreait\Firebase\Database;
 use Kreait\Firebase\Auth as FirebaseAuth;
 use Kreait\Firebase\Exception\FirebaseException;
 class RegisterController extends Controller {
-   use RegistersUsers;
-   protected $auth;
-   protected $redirectTo = RouteServiceProvider::HOME;
-   public function __construct(FirebaseAuth $auth) {
-      $this->middleware('guest');
-      $this->auth = $auth;
-   }
-   protected function validator(array $data) {
-      return Validator::make($data, [
-         'name' => ['required', 'string', 'max:255'],
-         'surname' => ['required', 'string', 'max:255'],
-         'email' => ['required', 'string', 'email', 'max:255'],
-         'password' => ['required', 'string', 'min:8', 'confirmed'],
-      ]);
-   }
    protected function register(Request $request) {
       try{
          $fullname = $request->input('name') . " " . $request->input('surname');
