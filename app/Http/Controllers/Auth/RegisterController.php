00797752d0e8449bd1c92ae90944aa6b5bb4d185
Anže Oblak <anze.oblak2@gmail.com>
2021-01-08 17:11:23 +0100

    Register Controller

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -29,29 +29,35 @@ class RegisterController extends Controller {
       ]);
    }
    protected function register(Request $request) {
-      $fullname = $request->input('name') . " " . $request->input('surname');
-      $userProperties = [
-         'email' => $request->input('email'),
-         'password' => $request->input('password'),
-         'displayName' => $fullname,
-         'sex' => $request->input('gender'),
-      ];
-      $database = app('firebase.database');
-      $oppositeSex;
-      if($userProperties['sex'] == "Male")
-         $oppositeSex = "Female";
-      else
-         $oppositeSex = "Male";
-      $database->getReference('Users/'.$uID.'')
-         ->set(
-            [
-               'name' => $userProperties['displayName'],
-               'profileImageUrl' => "https://firebasestorage.googleapis.com/v0/b/dateish-5d381.appspot.com/o/profileImages%2Fno-profile-picture-300x216.jpg?alt=media&token=be771306-e3fe-4826-bac7-606508fe64da",
-               'sex' => $userProperties['sex'],
-               'showSex' => $oppositeSex,
-               'minAge' => 18,
-               'maxAge' => 100,
-               'distance' => 142
-         ]);
+      try{
+         $fullname = $request->input('name') . " " . $request->input('surname');
+         $userProperties = [
+            'email' => $request->input('email'),
+            'password' => $request->input('password'),
+            'displayName' => $fullname,
+            'sex' => $request->input('gender'),
+         ];
+         $database = app('firebase.database');
+         $oppositeSex;
+         if($userProperties['sex'] == "Male")
+            $oppositeSex = "Female";
+         else
+            $oppositeSex = "Male";
+         $database->getReference('Users/'.$uID.'')
+            ->set(
+               [
+                  'name' => $userProperties['displayName'],
+                  'profileImageUrl' => "https://firebasestorage.googleapis.com/v0/b/dateish-5d381.appspot.com/o/profileImages%2Fno-profile-picture-300x216.jpg?alt=media&token=be771306-e3fe-4826-bac7-606508fe64da",
+                  'sex' => $userProperties['sex'],
+                  'showSex' => $oppositeSex,
+                  'minAge' => 18,
+                  'maxAge' => 100,
+                  'distance' => 142
+            ]);
+         return response("Sign up successful", 200);
+      }
+      catch(Exception $e){
+         return response('failed', 422);
+      }
    }
 }
